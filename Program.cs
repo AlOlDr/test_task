c265bad790a0033be0d811cbe359ce2a135bce5b
DiFF <alina.d.25@yandex.ru>
2022-09-23 21:24:29 +0300

    programm

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → [] -->
+
+
+// void PrintArray(string[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void StringsLessThenThree(string[] array1, string[] array2)
+// {
+//     int index = 0;
+//     for (int i = 0; i < array1.Length; i++)
+//     {
+//         if(array1[i].Length <= 3)
+//         {
+//             array2[index] = array1[i];
+//             index++;
+//         }
+//     }
+// }
+
+// string[] array3 = {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+// string[] array4 = new string[array3.Length];
+
+// StringsLessThenThree(array3, array4);
+// PrintArray(array4);
+
+
+string[] array1 = new string[11] {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+string[] array2 = new string[array1.Length];
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int index = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[index] = array1[i];
+        index++;
+        }
+    }
+}
+
+SecondArray(array1, array2);
+PrintArray(array2);
